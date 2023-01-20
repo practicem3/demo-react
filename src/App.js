84910bf7e61e29e33e0977f8279859b6a16d3247
unknown <practicem3@gmail.com>
2023-01-20 14:07:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hi Marcy</h1>
+        <h2>Hi</h2>
       </header>
     </div>
   );
