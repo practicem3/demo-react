114d45a44485d4cdf17bd3821414bff20670a82e
practicem3 <practicem3>
2023-01-20 14:21:00 +0530

    build update for gh-pages

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi Marcy</h1>
-        <h2>Hi</h2>
+        <h1>Hi Deepak</h1>
+        <h2>How are you Today</h2>
       </header>
     </div>
   );
